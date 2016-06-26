e0b78031ea30620b98be3425241bd78408c7da44
Hill <hill@kimo.com>
2016-06-26 10:05:45 +0000

    fixed unit test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 PMVC\Load::plug();
-PMVC\addPlugInFolder('../');
+PMVC\addPlugInFolders(['../']);
 class File_infoTest extends PHPUnit_Framework_TestCase
 {
     private $_plug = 'file_info';
