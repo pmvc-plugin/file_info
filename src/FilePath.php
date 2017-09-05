f3afda5ef4e0c7446ada0cb56c50f934deb9e6bc
Hill <hill@kimo.com>
2017-09-05 14:28:11 +0000

    fixed for unknown ext

--- src/FilePath.php
+++ src/FilePath.php
@@ -14,13 +14,16 @@ class FilePath
 
     function getExt()
     {
-        return $this->pathinfo['extension'];
+        return \PMVC\get($this->pathinfo, 'extension');
     }
 
     function getContentType()
     {
         $plug = \PMVC\plug('file_info');
-        return call_user_func([$plug, $this->getExt()])
-            ->getContentType();
+        $ext = $this->getExt();
+        if ($plug->isCallable($ext)) {
+            return call_user_func([$plug, $this->getExt()])
+                ->getContentType();
+        }
     }
 }
